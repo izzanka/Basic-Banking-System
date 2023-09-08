046a306fb0d4ac1c969d8c1af6067817b8fc0a78
izzanka <izzancoc.46@gmail.com>
2023-09-08 20:10:53 +0700

    menyalin file bank_account.js dari challange 1

---
+++ bank_account.js
@@ -0,0 +1,86 @@
+let saldo = 0;
+
+let txtTotalSaldo = document.createElement("p");
+let btnTambahSaldo = document.createElement("button");
+let btnKurangSaldo = document.createElement("button");
+
+txtTotalSaldo.innerHTML = `Total saldo: ${saldo}`;
+txtTotalSaldo.id = "txtTotalSaldo";
+document.body.appendChild(txtTotalSaldo);
+
+btnTambahSaldo.innerHTML = "Tambahkan saldo";
+btnTambahSaldo.onclick = () => tambahSaldo();
+document.body.appendChild(btnTambahSaldo);
+
+btnKurangSaldo.innerHTML = "Kurangi saldo";
+btnKurangSaldo.onclick = () => kurangiSaldo();
+document.body.appendChild(btnKurangSaldo);
+
+
+function tambahSaldo()
+{
+	let jumlahSaldoTambah = prompt("Masukkan jumlah saldo yang ingin ditambahkan", "0");
+
+	let hasilValidasi = validasiSaldo(jumlahSaldoTambah, "ditambahkan");
+
+	if(hasilValidasi)
+	{
+		saldo += hasilValidasi;
+		alert(`Penambahan saldo berhasil. Total saldo saat ini: ${saldo}`);
+		updateTextTotalSaldo();
+	}
+}
+
+function kurangiSaldo()
+{
+	let jumlahSaldoKurang = prompt("Masukkan jumlah saldo yang ingin dikurangi", "0");
+
+	let hasilValidasi = validasiSaldo(jumlahSaldoKurang, "dikurangi");
+
+	if(hasilValidasi)
+	{
+		saldo -= hasilValidasi;
+		alert(`Pengurangan saldo berhasil. Total saldo saat ini: ${saldo}`);
+		updateTextTotalSaldo();
+	}
+}
+
+function validasiSaldo(jumlahSaldo, keterangan)
+{	
+	if(!jumlahSaldo || !keterangan)
+	{
+		return;
+	}
+
+	let intJumlahSaldo = parseInt(jumlahSaldo);
+
+	if(isNaN(intJumlahSaldo))
+	{
+		alert(`Jumlah saldo yang ingin ${keterangan} harus berupa angka.`);
+		return;
+	}
+
+	if(intJumlahSaldo <= 0)
+	{
+		alert(`Jumlah saldo yang ingin ${keterangan} tidak boleh kurang dari atau sama dengan 0.`);
+	  	return;
+	}
+
+	if(keterangan == "dikurangi")
+	{
+		if(intJumlahSaldo > saldo)
+		{
+			alert("Saldo tidak mencukupi.");
+			return;
+		}
+	}
+
+	return intJumlahSaldo;
+}
+
+function updateTextTotalSaldo()
+{
+	let txtTotalSaldo = document.getElementById("txtTotalSaldo");
+	txtTotalSaldo.innerHTML = `Total saldo: ${saldo}`;
+}
+
